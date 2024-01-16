278b4d8df379d8aca791532eb7ded5a87b29c34b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-16 12:02:12 +0200

    filters

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,28 +1,46 @@
 // parsiusti ir sugeneruoti sarasa https://jsonplaceholder.typicode.com/posts
 
 import { useEffect, useState } from 'react';
-import TagsList from './TagsList';
 import SinglePost from './SinglePost';
 
 export default function Posts() {
   const [postsArr, setPostsArr] = useState([]);
-
+  const [selectValue, setSelectValue] = useState('10');
+  console.log('selectValue ===', selectValue);
   // getPosts(); // gausim infinite loop jei getPosts keicia state
+
+  function sortByLikes() {
+    console.log('sortByLikes');
+    // i setPostsArr funkcija paduoti postsArr versija kuri yra isrikiuota, nemodifikuojant postsArr
+    // shalow copy
+    // pasidarom kopija
+    const postArrCopy = [...postsArr];
+    // atliekam veikmus su kopija
+    postArrCopy.sort((aObj, bObj) => bObj.reactions - aObj.reactions);
+    // atnaujinam state su kopija
+    setPostsArr(postArrCopy);
+  }
+
+  function showOnly5() {
+    console.log('showOnly5');
+    // pasidaryti kopija
+    // su splice kukirpti ir palikti tik 5
+
+    // kurti nauja masyva
+    // sukti foreach ir sudeti tik pirmus 5 i nauja masyva
+
+    // arr.slice(0, 5)
+    const pirmi5 = postsArr.slice(0, 5);
+    setPostsArr(pirmi5);
+
+    // arr.filter()
+  }
+  // perdaryti i showOnly5(howMany){}
 
   useEffect(() => {
     getPostsDummy();
   }, []);
 
-  // function getPosts() {
-  //   fetch('https://jsonplaceholder.typicode.com/posts')
-  //     .then((resp) => resp.json())
-  //     .then((gotPostsArr) => {
-  //       setPostsArr(gotPostsArr.slice(0, 10));
-  //     })
-  //     .catch((error) => {
-  //       console.warn('ivyko klaida:', error);
-  //     });
-  // }
   function getPostsDummy() {
     fetch('https://dummyjson.com/posts')
       .then((resp) => resp.json())
@@ -35,9 +53,41 @@ export default function Posts() {
       });
   }
 
+  function handleInput(e) {
+    console.log('e.target.value ===', e.target.value);
+    let reiksme = e.target.value;
+  }
+
+  function handleSelectFilter(e) {
+    console.log('handleSelectFilter e.target.value ===', e.target.value);
+    let reiksme = e.target.value;
+    if (reiksme === '5') {
+      showOnly5();
+    }
+  }
+
   return (
     <div>
       <h2>Posts</h2>
+      <h2>tai kas ivesta: </h2>
+      <fieldset className='flex'>
+        <legend>Filters</legend>
+        <button onClick={sortByLikes} className='btn'>
+          Most liked posts
+        </button>
+        <button onClick={showOnly5} className='btn'>
+          show 5 posts
+        </button>
+        <input onChange={handleInput} type='text' placeholder='kazkas' />
+        <select onChange={handleSelectFilter} value={selectValue}>
+          <option disabled value=''>
+            How many to show
+          </option>
+          <option value='5'>5 posts</option>
+          <option value='10'>10 posts</option>
+          <option value='15'>15 posts</option>
+        </select>
+      </fieldset>
       <ul className='grid unlisted'>
         {postsArr.map((post) => (
           <SinglePost key={post.id} post={post} />
